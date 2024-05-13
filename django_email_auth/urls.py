5fe3d59c80d7679fcc6fa98754062d7bac29d49e
David Nugent <davidn@uniquode.io>
2024-05-13 20:34:21 +1000

    Remove files that don't belong in this module

--- django_email_auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import CustomLoginView, CustomLogoutView, CustomPasswordChangeView
-
-urlpatterns = [
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
-    path("change_password/", CustomPasswordChangeView.as_view(), name="change_password"),
-]
